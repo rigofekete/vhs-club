bf9d6d243260c130bb7dfc5efc8bd6dbd4181180
rigofekete <fabricecosteira@gmail.com>
2026-05-20 12:48:04 +0200

    chore: fix typo and remove unused mapper structs.

--- handler/rental_types.go
+++ handler/rental_types.go
@@ -6,18 +6,9 @@ import (
 	"github.com/google/uuid"
 )
 
-type CreateRentalRequest struct {
-	UserPublicID string `json:"user_id" binding:"required"`
-}
-
-type ReturnRentalRequest struct {
-	TapePublicID string `json:"tape_id" binding:"required"`
-	UserPublicID string `json:"user_id" binding:"required"`
-}
-
 type RentalResponse struct {
 	PublicID  uuid.UUID `json:"public_id"`
-	TapeID    int32     `jsong:"tape_id"`
+	TapeID    int32     `json:"tape_id"`
 	UserID    int32     `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	TapeTitle string    `json:"tape_title"`
